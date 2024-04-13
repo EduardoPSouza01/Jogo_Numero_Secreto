e86dbc371fcc652a90d54aebe587970455f6e0f5
Eduardo Souza <eduardo.p.p.souza@hotmail.com>
2024-04-13 18:15:44 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+let numMax = 100;
+let numeroSecreto = parseInt(Math.random() * (numMax + 1));
+let tentativas = 1;
+let chute;
+
+alert('Boas vindas ao jogo do número secreto');
+
+while (chute != numeroSecreto) {
+let chute = prompt(`Escolha um número entre 1 e ${numMax}`);
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert('O número secreto é menor');
+        } else {
+            alert('O número secreto é maior');
+        }
+    }
+    tentativas++;
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' ;
+alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} ${palavraTentativa}`);
